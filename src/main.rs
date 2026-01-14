9ce64c915b38f434faa1368b375d72249fae17f9
Marcell Ziegler <marcell.ziegler.22@gmail.com>
2026-01-14 15:55:49 +0100

    Base boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use crate::{model::App, ui::draw_ui};
+
+mod model;
+mod ui;
+
+fn main() -> color_eyre::Result<()> {
+    let mut term = ratatui::init();
+    let mut app = App::default();
+    term.draw(|frame| draw_ui(frame, &mut app))?;
+    ratatui::restore();
+    Ok(())
 }
---
+++ src/ui.rs
@@ -0,0 +1,7 @@
+use ratatui::Frame;
+
+use crate::model::App;
+
+pub fn draw_ui(frame: &mut Frame, app: &mut App) {
+    todo!();
+}
