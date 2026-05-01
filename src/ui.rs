4e0270bbbdc9819b89af8c227451c03ab8af5fed
Marcell Ziegler <marcell.ziegler.22@gmail.com>
2026-05-01 19:31:31 +0200

    Centered rect

--- src/ui.rs
+++ src/ui.rs
@@ -108,3 +108,20 @@ fn render_initiative_table(frame: &mut Frame, app: &mut App, area: Rect) {
 
     frame.render_stateful_widget(tab, area, &mut app.main_table_state);
 }
+
+/// Return a centered `Rect` area.
+fn centered_rect(percent_x: u16, percent_y: u16, area: Rect) -> Rect {
+    let popup_layout = Layout::vertical([
+        Constraint::Percentage((100 - percent_y) / 2),
+        Constraint::Percentage(percent_y),
+        Constraint::Percentage((100 - percent_y) / 2),
+    ])
+    .split(area);
+
+    Layout::horizontal([
+        Constraint::Percentage((100 - percent_x) / 2),
+        Constraint::Percentage(percent_x),
+        Constraint::Percentage((100 - percent_x) / 2),
+    ])
+    .split(popup_layout[1])[1]
+}
